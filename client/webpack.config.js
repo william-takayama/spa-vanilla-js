735dad2c33f7bc3f69a2feff75abbfdef0483349
William S. Takayama <60292132+william-takayama@users.noreply.github.com>
2020-11-02 17:40:36 -0300

    (Setup) Webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,10 +1,42 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   mode: "development",
+  devServer: {
+    open: true,
+    contentBase: "dist",
+  },
   entry: "./src/componentes/lista/listagem-cliente.js",
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/clientes.html",
+      filename: "index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[ext]",
+          },
+        },
+      },
+    ],
   },
 };
