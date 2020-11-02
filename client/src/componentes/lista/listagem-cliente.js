cf9fd25c87a36827050835d6a648dae1a6c3d233
William S. Takayama <60292132+william-takayama@users.noreply.github.com>
2020-11-02 17:41:31 -0300

    (Feat) Creating Components using vanillajs

--- client/src/componentes/lista/listagem-cliente.js
+++ client/src/componentes/lista/listagem-cliente.js
@@ -1,4 +1,6 @@
-import { deletaCliente, listarClientes } from "../../api/cliente.js";
+import { deletaCliente, listarClientes } from "../../api/cliente";
+import "../../assets/css/clientes.css";
+import inicializaCadastro from "../cadastro/componente-cadastro";
 
 const removeCliente = (id) => {
   if (confirm("Deseja deletar o cliente ?")) {
@@ -8,7 +10,32 @@ const removeCliente = (id) => {
   }
 };
 
-const corpoTabela = document.querySelector("[data-conteudo-tabela]");
+const conteudo = `
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">CPF</th>
+      <th scope="col">Nome</th>
+      <th scope="col"></th>
+      <th scope="col">
+        <a class="btn btn-primary">Novo Cliente</a>
+      </th>
+    </tr>
+  </thead>
+`;
+const container = document.querySelector("[data-container]");
+const tabela = document.createElement("table");
+
+tabela.innerHTML = conteudo;
+tabela.classList.add("table");
+container.appendChild(tabela);
+
+const novoCliente = document.querySelector(".btn");
+
+novoCliente.addEventListener("click", () => {
+  inicializaCadastro();
+});
+
+const corpoTabela = document.createElement("tbody");
 
 const exibeCliente = (cpf, nome, id) => {
   const linha = document.createElement("tr");
@@ -18,10 +45,8 @@ const exibeCliente = (cpf, nome, id) => {
     <td>${nome}</td>
     <button type="button" class="btn btn-danger" onclick="removeCliente(${id})">Excluir</button>
     <a href="./componentes/edita/edita-clientes.html?id=${id}">
-    <button type=""button class="btn btn-info">Editar</button>
+      <button type=""button class="btn btn-info">Editar</button>
     </a>
-    
-    
 `;
 
   linha.innerHTML = conteudoLinha;
@@ -33,3 +58,5 @@ listarClientes().then((exibe) => {
     corpoTabela.appendChild(exibeCliente(indice.cpf, indice.nome, indice.id));
   });
 });
+
+tabela.appendChild(corpoTabela);
